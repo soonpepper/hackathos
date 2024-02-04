99023087d972173130c0c2404f817c23218cd201
Vijayvenkat <vii.mjay@gmail.com>
2024-02-04 08:11:46 -0500

    fixed mag glass

--- mag.js
+++ mag.js
@@ -40,8 +40,8 @@ function magnify(imgID, zoom) {
     if (x < w / zoom) {x = w / zoom;}
     if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
     if (y < h / zoom) {y = h / zoom;}
-    glass.style.left = (x - w) + "px";
-    glass.style.top = (y - h) + "px";
+    //glass.style.left = (x - w) + "px";
+    //glass.style.top = (y - h) + "px";
     glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
   }
 
@@ -56,3 +56,6 @@ function magnify(imgID, zoom) {
     return {x : x, y : y};
   }
 }
+
+
+magnify("ifehead", 3);
