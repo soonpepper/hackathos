9cbeb8c03ab5b04fbba4a7fa730e64701607b6ae
TejaD0 <135356855+TejaD0@users.noreply.github.com>
2024-02-03 21:30:25 -0500

    Update mag.js

--- mag.js
+++ mag.js
@@ -35,12 +35,6 @@ function magnify(imgID, zoom) {
       x = pos.x;
       y = pos.y;
 
-      // Adjust the position of the magnifier glass to be centered on the cursor
-      if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
-      if (x < w / zoom) {x = w / zoom;}
-      if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
-      if (y < h / zoom) {y = h / zoom;}
-
       glass.style.left = (x - w) + "px";
       glass.style.top = (y - h) + "px";
       glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
