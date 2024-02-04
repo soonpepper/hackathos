3de1f458bf9ff63345452bc8d59b4d9f5e7bc137
TejaD0 <135356855+TejaD0@users.noreply.github.com>
2024-02-03 21:39:49 -0500

    Update mag.js

--- mag.js
+++ mag.js
@@ -56,8 +56,8 @@ function getCursorPos(e) {
   a = img.getBoundingClientRect();
 
   /* Calculate the cursor's x and y coordinates, relative to the image: */
-  x = e.pageX - 400;
-  y = e.pageY - 400;
+  x = e.pageX - 600;
+  y = e.pageY - 600;
 
   /* Consider any page scrolling: */
   x = x - window.pageXOffset;
